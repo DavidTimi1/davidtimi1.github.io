41ce834cad80770651a298c9779c39f0caf8f505
David <duwagbale07@gmail.com>
2025-01-04 06:19:05 +0100

    Feat (CTA && Services)

---
+++ src/components/CTA.jsx
@@ -0,0 +1,36 @@
+
+const CallToAction = () => {
+    return (
+        <>
+        <section className="pad box">
+
+            <Aesthetics />
+
+            <div className="mx-auto call-to-action br-1" style={{width: "min(100%, 600px)"}}>
+                <h3>Want to Collaborate ?</h3>
+                <button className="btn-primary" href=""  target="_blank" rel="noreferrer" >
+                    <div className="max">
+                        <span> Hit Me Up! </span>
+                    </div>
+                </button>
+            </div>
+        </section>
+
+        <hr className="mx-auto" style={{width: "min(90%, 500px)"}}></hr>
+        </>
+    );
+};
+
+export default CallToAction;
+
+
+
+
+const Aesthetics = () => (
+    <>
+        <div className="abs-mid" style={{aspectRatio: "1/1", borderRadius: "50%", backgroundColor: "#171a43", filter: "blur(150px)", transform: "translateY(-50%)", width: "300px"}}>
+        </div>
+        <div className="abs-mid" style={{aspectRatio: "1/1", borderRadius: "50%", backgroundColor: "#9f2ba6", filter: "blur(150px)", transform: "translateY(-50%)", left: 0, right: "50%", width: "300px"}}>
+        </div>
+    </>
+)
---
+++ src/components/Services.jsx
@@ -0,0 +1,46 @@
+
+const MyServices = () => {
+    return (
+        
+        <section className="my-services box fw">
+            <Aesthetics />
+
+			<div className="pad fw d-flex flex-column mid-align gap-5">
+                <h3>My Services</h3>
+                
+                <div className="services-grid fw">
+                    {
+                        services.map( (data, key) => <Service {...data} key={key} /> )
+                    }
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MyServices;
+
+
+const services = [
+    {title: "Website development", brief: "Build stunning websites with incredible conversion rates", icon: "/icon1.png"},
+    {title: "Webapp deployment", brief: "Deploy a scalable app to production in record time with ", icon: "/icon1.png"},
+    {title: "AI integration and bot engineering", brief: "Automate various parts of your product / service workflow, improving efficiency and customer support", icon: "/icon1.png"}
+]
+
+
+const Service = ({title, icon, brief}) => (
+    
+    
+    <div className="service-card">
+        <div className="max">
+            <h4>{title}</h4>
+            <p>{brief}</p>
+            <img src={icon} alt=""/>
+        </div>
+    </div> 
+)
+
+const Aesthetics = () => (
+    <div className="abs-mid" style={{aspectRatio: "1/1", borderRadius: "50%", backgroundColor: "#9f2ba6", filter: "blur(200px)", width: "300px"}}>
+    </div>
+)
